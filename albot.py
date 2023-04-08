c51364c27cfa377e64bd462efcada6f31b15bd7b
Anirudh Sriram <ani@college9-100-64-51-246.resnet.ucsc.edu>
2023-04-08 01:42:28 -0700

    Add functionality with gpt-3.5-turbo

--- albot.py
+++ albot.py
@@ -23,4 +23,22 @@ async def on_message(message):
     if message.content == 'ping':
         await message.channel.send('pong')
 
+    else:
+        print(message.content)
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                    #{"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": message.content},
+                    #{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+                    #{"role": "user", "content": "Where was it played?"}
+                ],
+            temperature=0.5,
+            max_tokens=150,
+            n=1,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+        await message.channel.send(response['choices'][0]['message']['content'])
+
 client.run(TOKEN)
