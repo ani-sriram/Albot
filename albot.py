d0c8cff51f1f279c2c6b37fb4783f4ea98b13738
Anirudh Sriram <ani@college9-100-64-51-246.resnet.ucsc.edu>
2023-04-09 12:53:17 -0700

    Chatbot functionality

--- albot.py
+++ albot.py
@@ -2,6 +2,8 @@ import os
 import discord
 import openai
 from dotenv import load_dotenv
+import typing
+import functools
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -15,30 +17,56 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
+
+def generate_response(messages):
+    response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=messages,
+    temperature=1,
+    max_tokens=50
+    )
+    return response.choices[0].text
+
+def translate_to_texting(message):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                {"role": "user", "content": "Rewrite the line below, removing proper grammar and punctuation. Keep your message short."},
+                {"role": "user", "content": message}
+            ],
+        temperature=0.5,
+        max_tokens=50,
+        n=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    return response['choices'][0]['message']['content']
+
+async def run_blocking(blocking_func: typing.Callable, *args, **kwargs) -> typing.Any:
+    func = functools.partial(blocking_func, *args, **kwargs)
+    return await client.loop.run_in_executor(None, func)
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if message.content == 'ping':
-        await message.channel.send('pong')
+        return message.channel.send('pong')
 
     else:
         print(message.content)
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                    #{"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": message.content},
-                    #{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-                    #{"role": "user", "content": "Where was it played?"}
-                ],
-            temperature=0.5,
-            max_tokens=150,
-            n=1,
-            frequency_penalty=0.0,
-            presence_penalty=0.0
-        )
-        await message.channel.send(response['choices'][0]['message']['content'])
+        author = []
+        content = []
+        messages = ""
+        async for msg in message.channel.history(limit=10):
+            if msg.content != "":
+                messages = ("%s: %s\n" % (msg.author.name, msg.content)) + messages
+        messages += "Albot: "
+        response = await run_blocking(generate_response, messages)
+        response = await run_blocking(translate_to_texting, response)
+    await message.channel.send(response) 
+
 
 client.run(TOKEN)
